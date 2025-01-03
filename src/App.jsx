7c8f522cf8da83f09887dcffe8af3b79600df771
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-03 20:16:10 +1100

    Updated App.jsx to include Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
+import Navbar from './components/Navbar';
 import './App.css'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<ShoppingCart />} />
