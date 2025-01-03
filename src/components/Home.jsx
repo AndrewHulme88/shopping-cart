06caf50b0d88e6e0123d546223372c010d6e2e7e
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-03 21:30:20 +1100

    Implemented fakestore api and shopping functionality to Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import shopping from '../assets/shopping.jpg';
+import axios from 'axios';
 
-const Home = () => {
+const Home = ({ addToCart }) => {
+  const [products, setProducts] = useState([]);
+  const [quantity, setQuantity] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+    .then(response => {
+      setProducts(response.data);
+    })
+    .catch(error => console.error("Error fetching products:", error));
+  }, []);
+
+  const handleQuantityChange = (productId, value) => {
+    setQuantity((prevQuantity) => ({
+      ...prevQuantity,
+      [productId]: value
+    }));
+  };
+
+  const increment = (productId) => {
+    setQuantity((prevQuantity) => ({
+      ...prevQuantity,
+      [productId]: (prevQuantity[productId] || 0) + 1
+    }));
+  };
+
+  const decrement = (productId) => {
+    setQuantity((prevQuantity) => ({
+      [productId]: Math.max((prevQuantity[productId] || 0) - 1, 0)
+    }));
+  };
+
+  const handleAddToCart = (product) => {
+    const productQuantity = quantity[productId] || 1;
+    addToCart(product, productQuantity);
+  };
+
   return (
     <div>
       <h2>Home</h2>
       <img src={shopping} alt="Shopping Image" />
+      <div className="product-list">
+        {products.map(product => (
+          <div key={product.id} className="product-card">
+            <h3>{product.title}</h3>
+            <img src={product.image} alt={product.title} />
+            <div>
+              <button onClick={() => decrement(product.id)}>-</button>
+              <input
+              type="number"
+              value={quantity[product.id] || 1}
+              onChange={(e) => handleQuantityChange(product.id, Number(e.target.value))}
+              min="1"
+               />
+               <button onClick={() => increment(product.id)}>+</button>
+            </div>
+            <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
