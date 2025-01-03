fb752ca9140b3f4b8c4aed406dd9735743fb1a29
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-03 20:38:39 +1100

    Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import shopping from '../assets/shopping.jpg';
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <img src={shopping} alt="Shopping Image" />
     </div>
   );
 }
