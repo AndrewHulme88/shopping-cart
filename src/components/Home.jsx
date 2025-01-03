4d641079a2e86f3242fd8fde1096ef4b2a6ad253
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-03 19:23:06 +1100

    Added links to home and cart pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Link to="/cart">Go to Shopping Cart</Link>
     </div>
   );
 }
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ShoppingCart = () => {
   return (
     <div>
       <h2>Your Shopping Cart</h2>
+      <Link to="/">Go Home</Link>
     </div>
   );
 }
